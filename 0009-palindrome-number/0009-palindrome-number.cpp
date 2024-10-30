55d6e8e1172c6c56bd16af4eded264619099a553
Giulio899 <101941123+Giulio899@users.noreply.github.com>
2024-10-30 17:34:50 +0100

    Time: 0 ms (100%), Space: 10.6 MB (13.37%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+
+        string number=to_string(x);
+
+        for(int i=0; i<number.size()/2; i++){
+            if(number[i]!=number[number.size()-1-i]) return false;
+        }
+
+        return true;
+    }
+
+};
